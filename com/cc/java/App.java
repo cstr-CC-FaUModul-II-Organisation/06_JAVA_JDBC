98eed6fb210df61a6697f449bd751df7438b4509
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-11-03 20:57:40 +0000

    Variante II / SELECT

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,30 +1,67 @@
 package com.cc.java;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class App {
 
+        // Argumente für Connection-Parameter
+        static String conURL = "jdbc:mysql://3.79.148.212:58020/mydb";
+        static String usrStr = "peter";
+        static String usrPwd = "abcd1234#";
+        
+        static Connection con;
+        static Statement stmt;
+        static ResultSet rs;
+
+
     public static void main(String[] args) {
 
-       String conURL = "jdbc:mysql://3.79.148.212:58848/mydb";
-       String usrStr = "peter";
-       String usrPwd = "abcd1234#";
-
-      try {
+        try {
+           
             // Load SQL Server JDBC driver and establish connection.
             System.out.print("Connecting to SQL Server ... ");
-            
+
             try (Connection con = DriverManager.getConnection(conURL, usrStr, usrPwd)) {
+
                 System.out.println("Success!");
+                
+                // Creating a statement object
+                Statement stmt = con.createStatement();
+
+                // Executing the query and storing the result in a ResultSet object
+                ResultSet rs = stmt.executeQuery("SELECT * FROM mydb.employees");
+
+                // Iterating through the ResultSet and printing the contents
+                while (rs.next()) {
+
+                    // fetch
+                    String name = rs.getString("name");
+                    String surname = rs.getString("surname");
+                    int age = rs.getInt("age");
+                    int salary = rs.getInt("salary");
+                    int employee_id = rs.getInt("employee_id");
+
+                    // print
+                    String printStr =   "ID: " + employee_id +
+                                        ", Name: " + surname + " " + name +
+                                        ", Salary: " + salary +
+                                        ", Age: " + age;
+                    System.out.println(printStr);
+                }
+
+                // Objekte schließen
+                rs.close();
+                stmt.close();
                 con.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
+
         } catch (Exception e) {
-            System.out.println();
             e.printStackTrace();
         }
-
 
     }
 }
